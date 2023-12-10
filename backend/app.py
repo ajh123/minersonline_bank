48630a2c02d7f50c361f15915b29b52f7b354967
ajh123 <ajh123@users.noreply.github.com>
2023-12-10 11:18:05 +0000

    feat(backend): allow users to be separate from accounts

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
+from typing import List
 from flask import Flask, request, jsonify
-from models import *
+from models import DataModel, Transaction, Account, Balance
 import json
 import jwt
 
@@ -39,6 +40,21 @@ def authenticate_user():
         return user_id
     return None
 
+def find_user_accounts(user_id) -> List[Account]:
+    user_accounts = []
+    for bank in data_model.banks:
+        for account in bank.accounts:
+            if account.owner.owner_id == user_id:
+                user_accounts.append(account)
+    return user_accounts
+
+def find_user_account(user_id, account_id):
+    for bank in data_model.banks:
+        for account in bank.accounts:
+            if account.owner.owner_id == user_id and account.account_id == account_id:
+                return account
+    return None
+
 def save_data():
     # Save DataModel back to JSON
     with open("data.json", "w") as file:
@@ -49,17 +65,11 @@ def save_data():
 def login():
     data = request.get_json()
 
-    for account in data_model.accounts:
-        if account.authentication:
-            for auth in account.authentication:
-                if (
-                    auth.type == "online_account"
-                    and auth.data["username"] == data["username"]
-                    and auth.data["password"] == data["password"]
-                ):
-                    user_id = account.account_id
-                    jwt_token = generate_jwt_token(user_id)
-                    return jsonify({"message": "Login successful", "jwt_token": jwt_token}), 200
+    for user in data_model.users:
+        if user.email == data["email"] and user.password == data["password"]:
+            user_id = user.user_id
+            jwt_token = generate_jwt_token(user_id)
+            return jsonify({"message": "Login successful", "jwt_token": jwt_token}), 200
 
     return jsonify({"error": "Invalid credentials"}), 401
 
@@ -67,85 +77,103 @@ def login():
 def view_balance():
     user_id = authenticate_user()
     if user_id:
-        current_user = next((account for account in data_model.accounts if account.account_id == user_id), None)
-        if current_user:
-            balances = [{"currency_id": balance.currency_id, "balance": balance.balance} for balance in current_user.balance]
+        user_accounts = find_user_accounts(user_id)
+        if user_accounts:
+            balances = []
+            for account in user_accounts:
+                balances.extend([{"account_id": account.account_id, "currency_id": balance.currency_id, "balance": balance.balance} for balance in account.balance])
             return jsonify({"balances": balances})
     return jsonify({"error": "User not authenticated"}), 401
+
+
+# ...
 
 @app.route("/transaction", methods=["POST"])
 def make_transaction():
     user_id = authenticate_user()
     if user_id:
-        current_user = next((account for account in data_model.accounts if account.account_id == user_id), None)
-        if current_user:
-            data = request.get_json()
+        data = request.get_json()
 
-            # Validate currency_id
-            currency_id_valid = any(balance.currency_id == data["currency_id"] for balance in current_user.balance)
-            if not currency_id_valid:
-                return jsonify({"error": "Invalid currency_id"}), 400
+        # Validate sender and recipient accounts
+        sender_account = find_user_account(user_id, data["from"])
+        recipient_account = find_user_account(user_id, data["to"])
 
-            # Validate recipient (to)
-            recipient = next((account for account in data_model.accounts if account.account_id == data["to"]), None)
-            if not recipient:
-                return jsonify({"error": "Invalid recipient (to)"}), 400
+        if not sender_account or not recipient_account:
+            return jsonify({"error": "Invalid sender or recipient account"}), 400
 
-            # Check if the user has enough balance
-            sender_balance = next((balance for balance in current_user.balance if balance.currency_id == data["currency_id"]), None)
-            if not sender_balance or sender_balance.balance < data["amount"]:
-                return jsonify({"error": "Insufficient balance"}), 400
+        # Validate ownership of the sender account
+        if sender_account.owner.owner_id != user_id:
+            return jsonify({"error": "User does not own the specified sender account"}), 401
 
-            # Calculate new balances
-            sender_balance = next((balance for balance in current_user.balance if balance.currency_id == data["currency_id"]), None)
-            recipient_balance = next((balance for balance in recipient.balance if balance.currency_id == data["currency_id"]), None)
+        # Validate specified currency in sender and recipient accounts
+        sender_currency = data.get("currency")
+        recipient_currency = sender_currency  # Assuming the same currency for simplicity
 
-            if not sender_balance or not recipient_balance:
-                return jsonify({"error": "Invalid balance data"}), 500
+        if not sender_currency or not recipient_currency:
+            return jsonify({"error": "Invalid or missing currency"}), 400
 
-            new_sender_balance = sender_balance.balance - data["amount"]
-            new_recipient_balance = recipient_balance.balance + data["amount"]
+        # Validate if both accounts can have the chosen currency
+        if not any(balance.currency_id == sender_currency for balance in sender_account.balance) or \
+           not any(balance.currency_id == recipient_currency for balance in recipient_account.balance):
+            return jsonify({"error": "Invalid currency for sender or recipient account"}), 400
 
-            # Update balances
-            sender_balance.balance = new_sender_balance
-            recipient_balance.balance = new_recipient_balance
+        # Validate if the user has enough balance in the sender account
+        sender_balance = next((balance.balance for balance in sender_account.balance if balance.currency_id == sender_currency), None)
+        if not sender_balance or sender_balance < data["amount"]:
+            return jsonify({"error": "Insufficient balance in the sender account"}), 400
 
-            # Create a new Transaction object for the sender
-            sender_transaction = Transaction(
-                from_id=current_user.account_id,
-                to_id=data["to"],
-                when=datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
-                currency_id=data["currency_id"],
-                previous_balance=sender_balance.balance + data["amount"],
-                new_balance=new_sender_balance
-            )
+        # Calculate new balances
+        new_sender_balance = sender_balance - data["amount"]
+        recipient_balance = next((balance.balance for balance in recipient_account.balance if balance.currency_id == recipient_currency), None)
+        if not recipient_balance:
+            return jsonify({"error": "Invalid recipient balance data"}), 500
 
-            # Create a new Transaction object for the recipient
-            recipient_transaction = Transaction(
-                from_id=current_user.account_id,
-                to_id=data["to"],
-                when=datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
-                currency_id=data["currency_id"],
-                previous_balance=recipient_balance.balance - data["amount"],
-                new_balance=new_recipient_balance
-            )
+        new_recipient_balance = recipient_balance + data["amount"]
 
-            current_user.transactions.append(sender_transaction)
-            recipient.transactions.append(recipient_transaction)
-            save_data()
+        # Update balances
+        sender_account.balance = [Balance(currency_id=sender_currency, balance=new_sender_balance)]
+        recipient_account.balance = [Balance(currency_id=recipient_currency, balance=new_recipient_balance)]
 
-            return jsonify({"message": "Transaction successful"}), 200
+        # Create a new Transaction object for the sender
+        sender_transaction = Transaction(
+            from_id=sender_account.account_id,
+            to_id=recipient_account.account_id,
+            when=datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+            currency_id=sender_currency,
+            previous_balance=sender_balance + data["amount"],
+            new_balance=new_sender_balance
+        )
+
+        # Create a new Transaction object for the recipient
+        recipient_transaction = Transaction(
+            from_id=sender_account.account_id,
+            to_id=recipient_account.account_id,
+            when=datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"),
+            currency_id=recipient_currency,
+            previous_balance=recipient_balance - data["amount"],
+            new_balance=new_recipient_balance
+        )
+
+        sender_account.transactions.append(sender_transaction)
+        recipient_account.transactions.append(recipient_transaction)
+        save_data()
+
+        return jsonify({"message": "Transaction successful"}), 200
 
     return jsonify({"error": "User not authenticated"}), 401
+
 
 @app.route("/messages")
 def view_messages():
     user_id = authenticate_user()
     if user_id:
-        current_user = next((account for account in data_model.accounts if account.account_id == user_id), None)
-        if current_user:
-            messages = [{"from": message.from_id, "data": message.data} for message in current_user.messages]
-            return jsonify({"messages": messages})
+        user_accounts = find_user_accounts(user_id)
+        messages = []
+
+        for account in user_accounts:
+            messages.extend([{"from": message.from_id, "data": message.data} for message in account.messages])
+
+        return jsonify({"messages": messages})
 
     return jsonify({"error": "User not authenticated"}), 401
 
--- backend/models.py
+++ backend/models.py
@@ -77,11 +77,11 @@ class Transaction:
 
     @classmethod
     def from_json(cls, data):
-        currency_data = data["currency"]
-        currency_id = currency_data["currency_id"]
-        previous_balance = currency_data["previous_balance"]
-        new_balance = currency_data["new_balance"]
-        return cls(data["from"], data["to"], data["when"], currency_id, previous_balance, new_balance)
+        currency_data = data.get("currency", {})
+        currency_id = currency_data.get("currency_id", "")
+        previous_balance = currency_data.get("previous_balance", 0)
+        new_balance = currency_data.get("new_balance", 0)
+        return cls(data.get("from", ""), data.get("to", ""), data.get("when", ""), currency_id, previous_balance, new_balance)
 
     def to_json(self):
         return {
@@ -95,12 +95,63 @@ class Transaction:
             }
         }
 
+class OwnerType:
+    def __init__(self, owner_type: str, owner_id: str):
+        self.type = owner_type
+        self.owner_id = owner_id
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(data.get("type", ""), data.get("owner_id", ""))
+
+    def to_json(self):
+        return {
+            "type": self.type,
+            "owner_id": self.owner_id
+        }
+
+class User:
+    def __init__(self, user_id: str, name: str, email: str, password: str, created_at: str):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.created_at = created_at
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(data["user_id"], data["name"], data["email"], data["password"], data["created_at"])
+
+    def to_json(self):
+        return {
+            "user_id": self.user_id,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "created_at": self.created_at
+        }
+
+class BankAdminAuthentication:
+    def __init__(self, user_id: str, permissions: List[str]):
+        self.user_id = user_id
+        self.permissions = permissions
+
+    @classmethod
+    def from_json(cls, data):
+        return cls(data.get("user_id", ""), data.get("permissions", []))
+
+    def to_json(self):
+        return {
+            "user_id": self.user_id,
+            "permissions": self.permissions
+        }
+
 class Account:
-    def __init__(self, account_id: str, bank_id: str, type: str, name: str, created_at: str, balance: List[Balance], messages: List[Message], transactions: List[Transaction], authentication: List[Authentication]):
+    def __init__(self, account_id: str, bank_id: str, type_id: str, owner: OwnerType, created_at: str, balance: List[Balance], messages: List[Message], transactions: List[Transaction], authentication: List[Authentication]):
         self.account_id = account_id
         self.bank_id = bank_id
-        self.type = type
-        self.name = name
+        self.type_id = type_id
+        self.owner = owner
         self.created_at = created_at
         self.balance = balance
         self.messages = messages
@@ -109,17 +160,20 @@ class Account:
 
     @classmethod
     def from_json(cls, data):
-        balance_data = data["balance"]
-        messages_data = data["messages"]
-        transactions_data = data["transactions"]
-        authentication_data = data["authentication"]
+        balance_data = data.get("balance", [])
+        messages_data = data.get("messages", [])
+        transactions_data = data.get("transactions", [])
+        authentication_data = data.get("authentication", [])
+
+        if authentication_data == None:
+            authentication_data = []
 
         return cls(
-            data["account_id"],
-            data["bank_id"],
-            data["type_id"],
-            data["name"],
-            data["created_at"],
+            data.get("account_id", ""),
+            data.get("bank_id", ""),
+            data.get("type_id", ""),
+            OwnerType.from_json(data.get("owner", {})),
+            data.get("created_at", ""),
             [Balance.from_json(b) for b in balance_data],
             [Message.from_json(m) for m in messages_data],
             [Transaction.from_json(t) for t in transactions_data],
@@ -130,8 +184,8 @@ class Account:
         return {
             "account_id": self.account_id,
             "bank_id": self.bank_id,
-            "type_id": self.type,
-            "name": self.name,
+            "type_id": self.type_id,
+            "owner": self.owner.to_json(),
             "created_at": self.created_at,
             "balance": [b.to_json() for b in self.balance],
             "messages": [m.to_json() for m in self.messages],
@@ -147,7 +201,7 @@ class AccountType:
 
     @classmethod
     def from_json(cls, data):
-        return cls(data["type_id"], data["name"], data["description"])
+        return cls(data.get("type_id", ""), data.get("name", ""), data.get("description", ""))
 
     def to_json(self):
         return {
@@ -156,46 +210,30 @@ class AccountType:
             "description": self.description
         }
 
-class BankAdminAuthentication:
-    def __init__(self, name: str, created_at: str, username: str, password: str):
-        self.name = name
-        self.created_at = created_at
-        self.username = username
-        self.password = password
-
-    @classmethod
-    def from_json(cls, data):
-        return cls(data["name"], data["created_at"], data["username"], data["password"])
-
-    def to_json(self):
-        return {
-            "name": self.name,
-            "created_at": self.created_at,
-            "username": self.username,
-            "password": self.password
-        }
-
 class Bank:
-    def __init__(self, bank_id: str, name: str, bank_type: str, created_at: str, account_types: List[AccountType], admin_authentication: List[BankAdminAuthentication]):
+    def __init__(self, bank_id: str, name: str, bank_type: str, created_at: str, account_types: List[AccountType], admin_authentication: List[BankAdminAuthentication], accounts: List[Account]):
         self.bank_id = bank_id
         self.name = name
         self.bank_type = bank_type
         self.created_at = created_at
         self.account_types = account_types
         self.admin_authentication = admin_authentication
+        self.accounts = accounts
 
     @classmethod
     def from_json(cls, data):
-        account_types_data = data["account_types"]
-        admin_authentication_data = data["admin_authentication"]
+        account_types_data = data.get("account_types", [])
+        admin_authentication_data = data.get("admin_authentication", [])
+        accounts_data = data.get("accounts", [])
 
         return cls(
-            data["bank_id"],
-            data["name"],
-            data["bank_type"],
-            data["created_at"],
+            data.get("bank_id", ""),
+            data.get("name", ""),
+            data.get("bank_type", ""),
+            data.get("created_at", ""),
             [AccountType.from_json(a) for a in account_types_data],
-            [BankAdminAuthentication.from_json(a) for a in admin_authentication_data]
+            [BankAdminAuthentication.from_json(a) for a in admin_authentication_data],
+            [Account.from_json(a) for a in accounts_data]
         )
 
     def to_json(self):
@@ -205,30 +243,31 @@ class Bank:
             "bank_type": self.bank_type,
             "created_at": self.created_at,
             "account_types": [a.to_json() for a in self.account_types],
-            "admin_authentication": [a.to_json() for a in self.admin_authentication]
+            "admin_authentication": [a.to_json() for a in self.admin_authentication],
+            "accounts": [a.to_json() for a in self.accounts]
         }
 
 class DataModel:
-    def __init__(self, accounts: List[Account], currencies: List[Currency], banks: List[Bank]):
-        self.accounts = accounts
+    def __init__(self, currencies: List[Currency], users: List[User], banks: List[Bank]):
         self.currencies = currencies
+        self.users = users
         self.banks = banks
 
     @classmethod
     def from_json(cls, data):
-        accounts_data = data["accounts"]
-        currencies_data = data["currencies"]
-        banks_data = data["banks"]
+        currencies_data = data.get("currencies", [])
+        users_data = data.get("users", [])
+        banks_data = data.get("banks", [])
 
         return cls(
-            [Account.from_json(a) for a in accounts_data],
             [Currency.from_json(c) for c in currencies_data],
+            [User.from_json(u) for u in users_data],
             [Bank.from_json(b) for b in banks_data]
         )
 
     def to_json(self):
         return {
-            "accounts": [a.to_json() for a in self.accounts],
             "currencies": [c.to_json() for c in self.currencies],
+            "users": [u.to_json() for u in self.users],
             "banks": [b.to_json() for b in self.banks]
         }
